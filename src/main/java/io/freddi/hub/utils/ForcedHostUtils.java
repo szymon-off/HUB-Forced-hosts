0f3c08b3d31d915e6e3c85f35d68d09f66fb2538
SzymON/OFF | Szymon Olszewik <szymon@olszewik.pl>
2025-07-09 20:04:43 +0200

    Add debug logs to ForcedHostUtils

--- src/main/java/io/freddi/hub/utils/ForcedHostUtils.java
+++ src/main/java/io/freddi/hub/utils/ForcedHostUtils.java
@@ -42,7 +42,7 @@ public class ForcedHostUtils extends Utils<ForcedHostUtils> {
     }
 
     public boolean isForcedHost(String host) {
-        logger.warn("Checking if host is forced: {}", host);
+        logger.debug("Checking if host is forced: {}", host); // temporary debug log
         return host != null && forcedHosts.contains(host.toLowerCase());
     }
 
